15c7d768bd2e62d86ce5707d48532b5f4c50869a
dldhkddl <a1994621@gmail.com>
2026-06-18 15:38:13 +0900

    에이팡 콤보 시스템

--- ai-mon/frontend/src/pages/Game/AipangPuzzle/assetPaths.js
+++ ai-mon/frontend/src/pages/Game/AipangPuzzle/assetPaths.js
@@ -5,17 +5,18 @@ import ap1 from '@aipang/ap1.png'
 import ap2 from '@aipang/ap2.png'
 import ap3 from '@aipang/ap3.png'
 import ap4 from '@aipang/ap4.png'
-import ap5 from '@aipang/ap5.png'
 import bossUnit  from '@aipang/boss_unit.png'
 import bossFinal from '@aipang/boss_final.png'
 import projStone from '@aipang/proj_stone.png'
+import fireballSrc from '@aipang/fireball.png'
 import bgmSrc    from '@aipang/bgm.m4a'
 import popSrc    from '@aipang/pop.mp3'
 
 /** 블록 이미지 배열: AP_IMGS[type] = URL (type 1~6) */
-export const AP_IMGS     = [null, ap1, ap2, ap3, ap4, ap5]
+export const AP_IMGS     = [null, ap1, ap2, ap3, ap4]
 export const BOSS_UNIT   = bossUnit
 export const BOSS_FINAL  = bossFinal
 export const PROJ_STONE  = projStone
+export const FIREBALL    = fireballSrc
 export const BGM_SRC     = bgmSrc
 export const POP_SRC     = popSrc
